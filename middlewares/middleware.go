e7aed6a6f62de622593ba387e37de8c63ace99ec
Paul Kerwin Acero <akosipoloyacero@gmail.com>
2021-02-24 00:43:38 +0400

    update on middleware

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"go-todo/models"
 	"log"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -50,4 +52,33 @@ func ExtractTokenClaims(tokenStr string) (jwt.MapClaims, bool) {
 		log.Printf("Invalid JWT Token")
 		return nil, false
 	}
+}
+
+func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Headers", r.Header)
+		reqToken := r.Header.Get("Authorization")
+		splitToken := strings.Split(reqToken, "Bearer ")
+		reqToken = splitToken[1]
+		if r.Header["Authorization"] != nil {
+			token, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return mySigningKey, nil
+			})
+
+			fmt.Println("TOKEN", token)
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
 }
--- server.go
+++ server.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 	"go-todo/controllers"
+	"go-todo/middlewares"
 	"log"
 	"net/http"
-	"strings"
-
-	"github.com/dgrijalva/jwt-go"
 )
 
 var mySigningKey = []byte("opensesame")
@@ -16,40 +14,11 @@ func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("test")
 }
 
-func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Headers", r.Header)
-		reqToken := r.Header.Get("Authorization")
-		splitToken := strings.Split(reqToken, "Bearer ")
-		reqToken = splitToken[1]
-		if r.Header["Authorization"] != nil {
-			token, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("There was an error")
-				}
-				return mySigningKey, nil
-			})
-
-			fmt.Println("TOKEN", token)
-
-			if err != nil {
-				fmt.Fprintf(w, err.Error())
-			}
-
-			if token.Valid {
-				endpoint(w, r)
-			}
-		} else {
-			fmt.Fprintf(w, "Not Authorized")
-		}
-	})
-}
-
 func main() {
 	fmt.Println("Hello World")
-	http.Handle("/tasks", isAuthorized(controllers.TaskHandler))
-	http.Handle("/tasks/", isAuthorized(controllers.TaskSpecificHandler))
+	http.Handle("/tasks", middlewares.IsAuthorized(controllers.TaskHandler))
+	http.Handle("/tasks/", middlewares.IsAuthorized(controllers.TaskSpecificHandler))
 	http.HandleFunc("/auth/", controllers.UserHandler)
-	http.Handle("/test", isAuthorized(test))
+	http.Handle("/test", middlewares.IsAuthorized(test))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
